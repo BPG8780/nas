f2413d8afe117054ea18b66440cb5d62213ba102
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-01-30 19:19:09 +0800

    Update nas.sh

--- nas.sh
+++ nas.sh
@@ -175,7 +175,7 @@ services:
       - /home/nginx/data:/data
       - /home/nginx/letsencrypt:/etc/letsencrypt
 EOF
-  echoContent yellow `echo -ne "请问是否安装Emby到本机《《《特别注意(Y/y)是ARM版本,(O/o)是AMD版本》》》[y/o/n]"`
+  echoContent yellow `echo -ne "请问是否安装Emby到本机《《《特别注意(1)是ARM版本,(2)是AMD版本》》》[y/o/n]"`
   read emby
   if [ "$emby" = "1" ]; then
     cat>>/root/docker-compose.yml <<EOF
@@ -251,7 +251,7 @@ EOF
     chmod +x /etc/init.d/check
     update-rc.d check defaults
     echoContent green "检测网盘挂载状态写入开机启动项完成···"
-    if [[ ${embyyno} == "Y" ]]||[[ ${embyyno} == "y" ]]; then
+    if [[ ${embyyn} == "Y" ]]||[[ ${embyyn} == "y" ]]; then
       echoContent green "qbittorrent端口8088（初始用户名admin，密码adminadmin）,nas-tools端口3000(默认用户名admin,密码password), nginx端口81(Email: admin@example.com,密码changeme)，Emby端口:8096"
     else
       echoContent green "qbittorrent端口8088（初始用户名admin，密码adminadmin, nas-tools端口3000(默认用户名admin,密码password), nginx端口81(Email: admin@example.com,密码changeme)"
