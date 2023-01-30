0f74306558a39cb7b18c7fafec68ce0d242f4173
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-01-30 18:25:11 +0800

    Update nas.sh

--- nas.sh
+++ nas.sh
@@ -197,6 +197,7 @@ EOF
       - /dev/dri:/dev/dri
     restart: unless-stopped
 EOF
+  else
   read embyyno
   if [[ ${embyyno} == "O" ]]||[[ ${embyynn} == "o" ]]; then
     cat>>/root/docker-compose.yml <<EOF
