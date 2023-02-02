8331d753d404b444e3227dd719c168851a1f4d18
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-02-02 17:15:51 +0800

    Update nas-tools.sh

--- nas-tools.sh
+++ nas-tools.sh
@@ -580,7 +580,7 @@ echoContent yellow "1. 一键安装Nas-tools
 2. 安装Rclone
 3. Rclone获取配置
 4. Rclone挂载网盘
-5. 甲骨文(龟壳）占用CPU不低于15%"
+5. 甲骨文(龟壳）占用CPU不低于10%"
   read -p "请选择输入菜单对应数字开始执行：" select_menu
   case "${select_menu}" in
     1)
