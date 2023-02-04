500671dfdd8d5a5f5afa4175e558f01f076228ba
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-02-04 13:47:34 +0800

    Update nas-tools.sh

--- nas-tools.sh
+++ nas-tools.sh
@@ -411,7 +411,7 @@ function insall_E5Sub(){
   elif [[ ${E5Sub} == "3" ]]; then
     wget --no-check-certificate -O /opt/e5sub/docker-compose.yml https://raw.githubusercontent.com/BPG8780/BPG/main/AMD/docker-compose.yml.example
   elif [[ ${E5Sub} == "4" ]]; then
-   cd /opt/e5sub && docker-compose up -d
+    cd /opt/e5sub && docker-compose up -d
     else
     echo
   fi
