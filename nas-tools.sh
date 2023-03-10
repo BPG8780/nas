0f67611506237abc7ddeb76b5aadd2da78d8c24e
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-03-10 23:31:09 +0800

    Update nas-tools.sh

--- nas-tools.sh
+++ nas-tools.sh
@@ -442,7 +442,7 @@ function insall_dujiaoka(){
   elif [[ ${dujiaoka} == "2" ]]; then
     wget --no-check-certificate -O /root/dujiaoka/docker-compose.yml https://ghproxy.com/https://raw.githubusercontent.com/BPG8780/dujiaoka/main/docker-compose.yml.example
   elif [[ ${dujiaoka} == "3" ]]; then
-    cd /dujiaoka && docker-compose up -d
+    docker-compose -f /root/dujiaoka/docker-compose.yml up -d
   elif [[ ${dujiaoka} == "4" ]]; then
     cd /dujiaoka && docker-compose down
     else
