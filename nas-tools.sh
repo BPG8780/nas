04df41c03a089f78f31b44f83976de82d3b361cf
BPG8780 <123814236+BPG8780@users.noreply.github.com>
2023-03-30 23:31:33 +0800

    Update nas-tools.sh

--- nas-tools.sh
+++ nas-tools.sh
@@ -47,7 +47,7 @@ function check_docker(){
   fi
   if test -z "$(which docker-compose)"; then
     echoContent yellow "检测到系统未安装docker-compose，开始安装docker-compose"
-    curl -L "https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    curl -L "https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
     ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
   fi
